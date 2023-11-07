f5fed2b3733f40903804ef647b3b4ca4249e0d89
unknown <gustavosmendes09@gmail.com>
2023-11-07 11:08:05 -0300

    Correcao problema m[0][0] = 0

--- matrixDet.cpp
+++ matrixDet.cpp
@@ -7,6 +7,7 @@ int main(){
 	printf("Informe a ordem:\nn = ");
 	scanf("%d",&n);
 	float matriz[n][n];
+	float det = 1;
 	
 	//Solicita valores para matriz
 	for(int i = 0; i < n; i++){
@@ -19,10 +20,80 @@ int main(){
 	//Escreve matriz
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < n; j++){
-			printf(" %f ",matriz[i][j]);
+			printf(" %.2f ",matriz[i][j]);
 		}
 		printf("\n");
-	} 
+	}
+	
+	
+	//Caso o fato [0,0] seja igual a 0
+		if(matriz[0][0] == 0){
+			float matrizAux[n][n];
+			int m = 1;
+			
+			//procura uma linha com pivo diferente de 0
+			for(int i=1; i < n;i++){
+				if(matriz[m][0] != 0){
+					i = n; //break
+				}
+				else{
+					m++;
+				}
+			}
+			//caso nao encontre, det = 0
+			if(m == 0){
+				det = 0;
+			}
+			else{
+				//Copia matriz em matrizAux
+				for(int i = 0; i < n; i++){
+					for(int j = 0; j < n; j++){
+						matrizAux[i][j] = matriz[i][j];
+					}
+				}
+				
+				//Salva na linha 0 os valores da linha M
+				for(int i = 0; i < n; i++){
+					matrizAux[0][i] = matrizAux[m][i];
+				}
+				
+				//Salva na linha M os valores da linha 0
+				for(int i = 0; i < n; i++){
+					matrizAux[m][i] = matriz[0][i];
+				}	
+				
+				
+				//Copia a nova matriz na original
+				for(int i = 0; i < n; i++){
+					for(int j = 0; j < n; j++){
+						matriz[i][j] = matrizAux[i][j];
+					}
+				}
+				
+				//Inverte as ultimas colunas, para manter o sinal
+				for(int i = 0; i < n; i++){
+					matrizAux[i][n-1] = matrizAux[i][n-2];
+				}
+				for(int i = 0; i < n; i++){
+					matrizAux[i][n-2] = matriz[i][n-1];
+				}
+				
+				//Copia a nova matriz na original
+				for(int i = 0; i < n; i++){
+					for(int j = 0; j < n; j++){
+						matriz[i][j] = matrizAux[i][j];
+					}
+				}
+			}
+		}
+	//Escreve matriz
+	printf("\nMatriz ajustada:\n");
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			printf(" %.2f ",matriz[i][j]);
+		}
+		printf("\n");
+	}
 	
 	
 	//Transforma matriz em diagonal inferior
@@ -36,19 +107,19 @@ int main(){
 	}
 	
 	//Calcula determinante
-	float det = 1;
 	for(int i; i < n; i++){
 		det = det * matriz[i][i];
 	}
+	if(n ==0)det = 0;
 	
 	//Imprime matriz nova
 	printf("\nMatriz novamente:\n");
 	
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < n; j++){
-			printf(" %f ",matriz[i][j]);
+			printf(" %.2f ",matriz[i][j]);
 		}
 		printf("\n");
 	}
-	printf("det: %f",det);
+	printf("det: %.3f",det);
 }
